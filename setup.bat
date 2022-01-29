6e8b08ebd81b62fd9147ac78c1e66761e3e80d49
OriginalAlien <80835991+OriginalAlien@users.noreply.github.com>
2022-01-28 23:55:46 -0500

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0A
+cls
+echo Downloading Modules...
+python -m pip install colorama
+python -m pip install selenium
+python -m pip install time
+python -m pip install sys
+python -m pip install os
+echo Done
+pause
