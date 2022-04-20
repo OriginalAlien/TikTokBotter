62d1f9d6cdc17532c9fea37b77f11253d036d108
OriginalAlien <80835991+OriginalAlien@users.noreply.github.com>
2022-04-20 12:00:54 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,7 +5,6 @@ echo Downloading Modules...
 python -m pip install colorama
 python -m pip install selenium
 python -m pip install time
-python -m pip install sys
 python -m pip install os
 echo Done
 pause
